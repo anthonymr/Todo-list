a69d77ccda8d349561a11c16950795bcd0cfcd0b
Anthony Martin <anthonymartin20@gmail.com>
2023-01-16 22:26:33 +0100

    Create Task class

---
+++ src/modules/task.js
@@ -0,0 +1,28 @@
+export default class task {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+
+  draw(){
+    const li = document.createElement('li');
+    li.classList.add('box');
+
+    const chk = document.createElement('input');
+    chk.setAttribute('type', 'checkbox'); 
+    chk.checked = this.completed;
+
+    const span = document.createElement('span');
+    span.innerHTML = this.description;
+
+    const icon = document.createElement('i');
+    icon.classList.add('fa-solid', 'fa-ellipsis-vertical');
+
+    li.appendChild(chk);
+    li.appendChild(span);
+    li.appendChild(icon);
+
+    return li;
+  }
+}
