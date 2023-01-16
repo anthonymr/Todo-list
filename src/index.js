8379a616af16443b44b88a2179659e2bd806fbac
Anthony Martin <anthonymartin20@gmail.com>
2023-01-16 22:27:35 +0100

    Create JS entry point

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import TodoList from "./modules/todoList.js";
+import './index.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+
+const myList = new TodoList('ul');
+
+myList.addTask('wash the dishes');
+myList.addTask('complete To Do list project');
