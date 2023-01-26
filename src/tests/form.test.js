49b6e84857e4bddb74db83e0d43111f9331a6038
Anthony Martin <anthonymartin20@gmail.com>
2023-01-26 08:55:28 +0100

    Solve linters problem 1

--- src/tests/form.test.js
+++ src/tests/form.test.js
@@ -31,109 +31,109 @@ const testEventStorage = new Map();
 let testEventOutput = 0;
 
 const testEvent = {
-    dataTransfer: {
-      setData: (key, value) => testEventStorage.set(key, value),
-      getData: (key) => testEventStorage.get(key)
-    },
-    preventDefault: () => testEventOutput = 1
+  dataTransfer: {
+    setData: (key, value) => testEventStorage.set(key, value),
+    getData: (key) => testEventStorage.get(key)
+  },
+  preventDefault: () => testEventOutput = 1
 };
 
 describe('Optional requirements', () => {
-    test('Add event must create new task', () => {
-        const event = { key: 'Enter', type: 'click' }
+  test('Add event must create new task', () => {
+    const event = { key: 'Enter', type: 'click' }
 
-        Form.addInput = { value: 'just a test' };
-        Form.addEvent(event);
-        
-        expect(Form.list.tasks).toHaveLength(1);
-    });
+    Form.addInput = { value: 'just a test' };
+    Form.addEvent(event);
 
-    test('Remove event must remove task', () => {
-        Form.index = 1;
-        Form.removeEvent();
-        
-        expect(Form.list.tasks).toHaveLength(0);
-    });
+    expect(Form.list.tasks).toHaveLength(1);
+  });
 
-    test('toggleCompleted event must change task complete status', () => {
-        const event = { key: 'Enter', type: 'click' }
+  test('Remove event must remove task', () => {
+    Form.index = 1;
+    Form.removeEvent();
 
-        Form.addInput = { value: 'just a test' };
-        Form.addEvent(event);
+    expect(Form.list.tasks).toHaveLength(0);
+  });
 
-        const toggleEvent = { currentTarget: { checked: true } };
+  test('toggleCompleted event must change task complete status', () => {
+    const event = { key: 'Enter', type: 'click' }
 
-        Form.list.tasks[0].completed = true;
+    Form.addInput = { value: 'just a test' };
+    Form.addEvent(event);
 
-        Form.toggleCompleted(toggleEvent)
+    const toggleEvent = { currentTarget: { checked: true } };
 
-        expect(Form.list.tasks[0].domCheck.checked).toBe(true);
-    });
+    Form.list.tasks[0].completed = true;
 
-    test('toggleCompleted event must change task complete status', () => {
-        const event = { key: 'Enter', type: 'click' }
+    Form.toggleCompleted(toggleEvent)
 
-        Form.addInput = { value: 'just a test 2' };
-        Form.addEvent(event);
+    expect(Form.list.tasks[0].domCheck.checked).toBe(true);
+  });
 
-        const toggleEvent = { currentTarget: { checked: false } };
+  test('toggleCompleted event must change task complete status', () => {
+    const event = { key: 'Enter', type: 'click' }
 
-        Form.list.tasks[0].completed = false;
+    Form.addInput = { value: 'just a test 2' };
+    Form.addEvent(event);
 
-        Form.toggleCompleted(toggleEvent)
+    const toggleEvent = { currentTarget: { checked: false } };
 
-        expect(Form.list.tasks[0].domCheck.checked).toBe(false);
-    });
+    Form.list.tasks[0].completed = false;
 
-    test('edit event must add .editing css class', () => {
-        Form.editing = false;
-        Form.domElement = Form.list.tasks[0].domElement;
-        Form.domInput = Form.list.tasks[0].domInput;
-        Form.domSpan = Form.list.tasks[0].domSpan;
+    Form.toggleCompleted(toggleEvent)
 
-        Form.editEvent();
+    expect(Form.list.tasks[0].domCheck.checked).toBe(false);
+  });
 
-        expect(Form.editing).toBe(true);
-        expect(Form.list.tasks[0].domElement.classList.contains('editing')).toBe(true);
-    });
+  test('edit event must add .editing css class', () => {
+    Form.editing = false;
+    Form.domElement = Form.list.tasks[0].domElement;
+    Form.domInput = Form.list.tasks[0].domInput;
+    Form.domSpan = Form.list.tasks[0].domSpan;
 
-    test('edit event must do nothing when already editing', () => {
-        Form.editing = true;
+    Form.editEvent();
 
-        Form.editEvent();
+    expect(Form.editing).toBe(true);
+    expect(Form.list.tasks[0].domElement.classList.contains('editing')).toBe(true);
+  });
 
-        expect(Form.list.tasks[0].domElement.classList.contains('editing')).toBe(true);
-    });
+  test('edit event must do nothing when already editing', () => {
+    Form.editing = true;
 
-    test('dragTask must set index value', () => {
-        Form.dragTask(testEvent);
+    Form.editEvent();
 
-        expect(testEvent.dataTransfer.getData('index')).toBe(1);
-        
-    });
+    expect(Form.list.tasks[0].domElement.classList.contains('editing')).toBe(true);
+  });
 
-    test('allowDropTask must prevent Default behavior', () => {
-        Form.allowDropTask(testEvent)
-        expect(testEventOutput).toBe(1);
-    });
+  test('dragTask must set index value', () => {
+    Form.dragTask(testEvent);
 
-    test('dropTask must switch item index', () => {
-        Form.index = 2;
-        Form.dropTask(testEvent);
-        expect(Form.list.tasks[0].description).toBe('just a test 2');     
-        expect(Form.list.tasks[1].description).toBe('just a test');     
-    });
+    expect(testEvent.dataTransfer.getData('index')).toBe(1);
 
-    test('keypress enter event on task must remove editing css class', () => {
-        Form.editing = false;
-        Form.domElement = Form.list.tasks[0].domElement;
-        Form.domInput = Form.list.tasks[0].domInput;
-        Form.domSpan = Form.list.tasks[0].domSpan;
+  });
 
-        Form.editEvent();
+  test('allowDropTask must prevent Default behavior', () => {
+    Form.allowDropTask(testEvent)
+    expect(testEventOutput).toBe(1);
+  });
 
-        expect(Form.list.tasks[0].domElement.classList.contains('editing')).toBe(true);
-        Form.list.tasks[0].domInput.dispatchEvent(new KeyboardEvent('keypress', {'key': 'Enter'}));
-        expect(Form.list.tasks[0].domElement.classList.contains('editing')).toBe(false);        
-    });
+  test('dropTask must switch item index', () => {
+    Form.index = 2;
+    Form.dropTask(testEvent);
+    expect(Form.list.tasks[0].description).toBe('just a test 2');
+    expect(Form.list.tasks[1].description).toBe('just a test');
+  });
+
+  test('keypress enter event on task must remove editing css class', () => {
+    Form.editing = false;
+    Form.domElement = Form.list.tasks[0].domElement;
+    Form.domInput = Form.list.tasks[0].domInput;
+    Form.domSpan = Form.list.tasks[0].domSpan;
+
+    Form.editEvent();
+
+    expect(Form.list.tasks[0].domElement.classList.contains('editing')).toBe(true);
+    Form.list.tasks[0].domInput.dispatchEvent(new KeyboardEvent('keypress', { 'key': 'Enter' } ));
+    expect(Form.list.tasks[0].domElement.classList.contains('editing')).toBe(false);
+  });
 });
